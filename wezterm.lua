3a9a2f095e43f94593cf2da0fc1b783925a261d5
Ryan Butler <thebutlah@gmail.com>
2023-08-23 12:31:50 -0400

    Only use wsl on windows

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-        config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -15,7 +15,11 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 
-config.default_domain = "WSL:Ubuntu-20.04"
+if package.config:sub(1, 1) == '\\' then
+	-- We are on windows
+	config.default_domain = "WSL:Ubuntu-20.04"
+end
+
 config.window_decorations = "RESIZE | INTEGRATED_BUTTONS"
 config.window_padding = { left = 0, right = 0, top = "0.2cell", bottom = "0.2cell" }
 
