b82c827b736db6c558e66f85bf8358ed733cf399
Ryan Butler <thebutlah@gmail.com>
2024-03-30 19:33:29 +0000

    mkdir -p wezterm and alacritty

--- user-setup.sh
+++ user-setup.sh
@@ -21,7 +21,9 @@ echo 'set -o vi' >> ~/.zshrc
 echo 'set -o vi' >> ~/.bash_profile
 
 ln -sf ${HOME}/P/nix/starship.toml ${HOME}/.config/starship.toml
+mkdir -p ~/.config/wezterm
 ln -sf ${HOME}/P/nix/wezterm.lua ${HOME}/.config/wezterm/wezterm.lua
+mkdir -p ~/.config/alacritty
 ln -sf ${HOME}/P/nix/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
