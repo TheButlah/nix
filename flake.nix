967d31e3d9b235082d7080f646abfe8efe9d1ec5
Ryan Butler <thebutlah@gmail.com>
2024-04-01 17:16:30 -0400

    Downgrade to nixpkgs-23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TheButlah's personal dev environment";
   inputs = {
     # Worlds largest repository of linux software
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # Provides eachDefaultSystem and other utility functions
     utils.url = "github:numtide/flake-utils";
     # Replacement for rustup
