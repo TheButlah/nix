aeed7999ac5b44f0457cb1a45822bb9389a99130
Ryan Butler <thebutlah@gmail.com>
2024-06-27 19:06:22 -0400

    Fix OP env var path

--- home.nix
+++ home.nix
@@ -47,7 +47,7 @@ in
       set -o vi
     '';
     envExtra = ''
-      export OPENAI_API_KEY="''${OPENAI_API_KEY:-"$(op read --account PJ5RFQLTJNBQDI3OMBJHR3LOZ4 "op://Personal/OpenAI API Key/credential")"}"
+      export OPENAI_API_KEY="''${OPENAI_API_KEY:-"$(op read --account PJ5RFQLTJNBQDI3OMBJHR3LOZ4 "op://Private/OpenAI API Key/credential")"}"
     '';
   };
   programs.starship = {
