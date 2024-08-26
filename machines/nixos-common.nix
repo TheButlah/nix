ef8b2255ea079e5bb5378319f8bf3de682f3b7ce
Ryan Butler <thebutlah@gmail.com>
2024-08-26 05:32:08 -0400

    added unprivileged_userns_clone

--- machines/nixos-common.nix
+++ machines/nixos-common.nix
@@ -82,9 +82,15 @@ in
   };
 
   # use the latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  # Needed for https://github.com/NixOS/nixpkgs/issues/58959
-  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernel.sysctl = {
+      # Needed to run buildFHSEnv in github runner
+      "kernel.unprivileged_userns_clone" = 1;
+    };
+    # Needed for https://github.com/NixOS/nixpkgs/issues/58959
+    supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  };
 
   # Enable networking
   networking.networkmanager.enable = true;
