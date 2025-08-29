0807dd9a18b527c972f2a3bcbed73580f56ea1eb
Ryan Butler <thebutlah@gmail.com>
2025-08-28 23:50:52 -0400

    enable xwayland-satellite by default, add proton-ge to steam

--- machines/ryan-desktop/configuration.nix
+++ machines/ryan-desktop/configuration.nix
@@ -331,6 +331,9 @@ in
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     gamescopeSession.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
   programs.gamescope = {
     enable = true;
