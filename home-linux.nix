63c993b551971db60d0db9a0d07746f5704d51a3
Ryan Butler <thebutlah@gmail.com>
2025-09-03 12:38:04 -0400

    added swayidle

--- home-linux.nix
+++ home-linux.nix
@@ -35,6 +35,60 @@ lib.mkIf pkgs.stdenv.isLinux {
   services.mako = {
     enable = isGui && isWayland;
   };
+  services.swayidle =
+    # from https://wiki.nixos.org/wiki/Swayidle
+    let
+      # Lock command
+      lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
+      # TODO: modify "display" function based on your window manager
+      # Sway
+      # display = status: "swaymsg 'output * power ${status}'"; \
+      # Hyprland
+      # display = status: "hyprctl dispatch dpms ${status}";
+      # Niri
+      display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+    in
+    {
+      enable = true;
+      timeouts = [
+        {
+          timeout = 60 * 2; # in seconds
+          command = "${pkgs.libnotify}/bin/notify-send 'Locking in 30 seconds' -t 30000";
+        }
+        {
+          timeout = 60 * 2 + 30;
+          command = lock;
+        }
+        {
+          timeout = 60 * 3;
+          command = display "off";
+          resumeCommand = display "on";
+        }
+        {
+          timeout = 60 * 20;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          # adding duplicated entries for the same event may not work
+          command = (display "off") + "; " + lock;
+        }
+        {
+          event = "after-resume";
+          command = display "on";
+        }
+        {
+          event = "lock";
+          command = (display "off") + "; " + lock;
+        }
+        {
+          event = "unlock";
+          command = display "on";
+        }
+      ];
+    };
 
   # note: run `spotifyd authenticate` to login.
   services.spotifyd = {
