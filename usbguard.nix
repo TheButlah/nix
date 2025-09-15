c125d8402c1b77d4b7508c1edeb1b27f5524b6eb
Ryan Butler <thebutlah@gmail.com>
2025-09-15 18:47:02 -0400

    allowlist AT2020+ mic

--- usbguard.nix
+++ usbguard.nix
@@ -1,11 +1,14 @@
 {
   enable = true;
   rules = ''
+    # Generic classes
     allow with-interface equals { 09:00:* } # usb hubs
     allow with-interface equals { 08:*:* } # Mass storage
     allow with-interface equals { 08:*:* 08:*:* } # Mass storage with multiple protocols
 
+    # Specific devices
     allow id 1050:0402 serial "" name "YubiKey FIDO" hash "ZZ2J/ZgNx0xMmsjwUrSVnZ4BdrM/RSDsfjrYRCmvoME=" with-interface 03:00:00
+    allow id 0909:001c serial "" name "AT2020USB+" hash "/Rj52epXXbngHHurEDmdeKQOn/F9aYSO4ChFo/Snpgo=" with-interface { 01:01:00 01:02:00 01:02:00 01:02:00 01:02:00 03:00:00 }
 
     # Block everything else
     block id *:*
