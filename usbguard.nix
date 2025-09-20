f1d204037d212fd4960b8f7f716f259457ec95ad
Ryan Butler <thebutlah@gmail.com>
2025-09-19 21:25:09 -0400

    added digitakt ii to usbguard

--- usbguard.nix
+++ usbguard.nix
@@ -9,6 +9,7 @@
     # Specific devices
     allow id 1050:0402 serial "" name "YubiKey FIDO" hash "ZZ2J/ZgNx0xMmsjwUrSVnZ4BdrM/RSDsfjrYRCmvoME=" with-interface 03:00:00
     allow id 0909:001c serial "" name "AT2020USB+" hash "/Rj52epXXbngHHurEDmdeKQOn/F9aYSO4ChFo/Snpgo=" with-interface { 01:01:00 01:02:00 01:02:00 01:02:00 01:02:00 03:00:00 }
+    allow id 1935:102b serial "000000000001" name "Elektron Digitakt II" hash "qhmC0G+vsuzFsGWypvwiQ99fqIXgYejjChjBO6hcx3I=" with-interface { 01:01:20 01:02:20 01:02:20 01:02:20 01:02:20 01:01:00 01:03:00 }
 
     # Block everything else
     block id *:*
