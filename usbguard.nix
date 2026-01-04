05c03c1d64f032d6cebd6e6b553ca5895ef771e0
Ryan Butler <thebutlah@gmail.com>
2026-01-03 21:04:16 -0500

    allow pixel 10

--- usbguard.nix
+++ usbguard.nix
@@ -13,6 +13,7 @@
     allow id 0909:001c serial "" name "AT2020USB+" hash "/Rj52epXXbngHHurEDmdeKQOn/F9aYSO4ChFo/Snpgo=" with-interface { 01:01:00 01:02:00 01:02:00 01:02:00 01:02:00 03:00:00 }
     allow id 1935:102b serial "000000000001" name "Elektron Digitakt II" hash "qhmC0G+vsuzFsGWypvwiQ99fqIXgYejjChjBO6hcx3I=" with-interface { 01:01:20 01:02:20 01:02:20 01:02:20 01:02:20 01:01:00 01:03:00 }
     allow id 1d50:615e serial "7E9C97C092E11E0A" name "Corne" hash "F1i6fSRQyssfm7PAtDBNxR3tzdarO+2Gz0aEjz5/9Og=" with-interface 03:00:00
+    allow id 18d1:4ee7 serial "58090DLCQ0093E" name "Pixel 10 Pro XL" hash "aoBAkQgPrRbdl2CN9oDebYxvbZlLogW8Sudy6Vw7wfA=" parent-hash "zPEgphy8DinLP2QQPlh78r65kXqs8tTFSkoXXpgrIC8=" with-interface ff:42:01
 
     # Block everything else
     block id *:*
