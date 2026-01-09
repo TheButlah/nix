ef994d44c2eb091e757ffb6d2152f4bcbc97461b
Ryan Butler <thebutlah@gmail.com>
2026-01-08 21:25:23 -0500

    added ftdi serial and nvidia pearl to usbguard

--- usbguard.nix
+++ usbguard.nix
@@ -14,6 +14,8 @@
     allow id 1935:102b serial "000000000001" name "Elektron Digitakt II" hash "qhmC0G+vsuzFsGWypvwiQ99fqIXgYejjChjBO6hcx3I=" with-interface { 01:01:20 01:02:20 01:02:20 01:02:20 01:02:20 01:01:00 01:03:00 }
     allow id 1d50:615e serial "7E9C97C092E11E0A" name "Corne" hash "F1i6fSRQyssfm7PAtDBNxR3tzdarO+2Gz0aEjz5/9Og=" with-interface 03:00:00
     allow id 18d1:4ee7 serial "58090DLCQ0093E" name "Pixel 10 Pro XL" hash "aoBAkQgPrRbdl2CN9oDebYxvbZlLogW8Sudy6Vw7wfA=" parent-hash "zPEgphy8DinLP2QQPlh78r65kXqs8tTFSkoXXpgrIC8=" with-interface ff:42:01
+    allow id 0403:6001 name "FT232R USB UART" with-interface ff:ff:ff with-connect-type "unknown"
+    allow id 0955:7e19 name "APX" with-interface ff:ff:ff with-connect-type "unknown" # Jetson
 
     # Block everything else
     block id *:*
