c381c449b92e7480da4e21f5df06b6c0637d3353
Ryan Butler <thebutlah@gmail.com>
2026-09-07 22:43:50 -0400

    fix OLLAMA_HOST ryan-desktop

--- machines/ryan-desktop/configuration.nix
+++ machines/ryan-desktop/configuration.nix
@@ -230,6 +230,7 @@ in
     package = pkgs.unstable.ollama-cuda;
     host = "100.94.243.29";
   };
+  environment.sessionVariables.OLLAMA_HOST = config.services.ollama.host;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
